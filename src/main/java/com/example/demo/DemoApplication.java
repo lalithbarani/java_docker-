c211372c8112d7f40066fcb5092bbbab1e7943c9
lalithbarani <130025078+lalithbarani@users.noreply.github.com>
2024-07-03 18:34:18 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+	@GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 
 }
