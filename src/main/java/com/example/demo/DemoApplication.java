71128125dbccc7be5b6b9160125e046d90adb78b
lalithbarani <130025078+lalithbarani@users.noreply.github.com>
2024-07-04 17:38:53 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,13 +3,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	@GetMapping("/hello")
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
-
 }
